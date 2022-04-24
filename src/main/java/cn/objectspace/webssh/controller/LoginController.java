eb120cf83711c481c8163e5f048dd5c2844a305a
wucw <wucw@zoom.com>
2022-04-24 18:58:08 +0800

    feat:host

--- src/main/java/cn/objectspace/webssh/controller/LoginController.java
+++ src/main/java/cn/objectspace/webssh/controller/LoginController.java
@@ -1,6 +1,13 @@
 package cn.objectspace.webssh.controller;
 
 
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.Base64Utils;
@@ -15,11 +22,13 @@ import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Properties;
 
 @RequestMapping("/wss")
 @Controller
 public class LoginController {
 
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
     @RequestMapping("login")
     public String login(HttpServletResponse response) {
@@ -28,27 +37,50 @@ public class LoginController {
 
     @ResponseBody
     @RequestMapping("loginSub")
-    public ResponseEntity loginSub(@RequestParam  Map<String,String> map, HttpServletResponse response, HttpSession session) {
+    public ResponseEntity loginSub(@RequestParam  Map<String,String> map, HttpServletResponse response
+                                   ) {
         String host = map.get("host");
         String username = map.get("username");
         String port = map.get("port");
         String password = map.get("password");
-        session.setAttribute("username", username);
         String encodeToString = Base64Utils.encodeToString(password.getBytes(StandardCharsets.UTF_8));
-        session.setAttribute("password", encodeToString);
-
-        Cookie cookie = new Cookie("username", username);
-        cookie.setPath("/");
-        cookie.setMaxAge(30 * 60 * 1000);
-        Cookie passwordCookie = new Cookie("password", encodeToString);
-        passwordCookie.setPath("/");
-        passwordCookie.setMaxAge(30 * 60 * 1000);
-        response.addCookie(cookie);
-        response.addCookie(passwordCookie);
-        Map maps = new HashMap();
+        if (StringUtils.isAnyBlank(username, host, password, port)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        Map<String,Object> maps = new HashMap<>();
         maps.put("host", host);
         maps.put("port", port);
         maps.put("username", username);
+        Session session = null;
+        try {
+            JSch jSch = new JSch();
+            Properties config = new Properties();
+            config.put("StrictHostKeyChecking", "no");
+            //获取jsch的会话
+            session  = jSch.getSession(username, host, Integer.parseInt(port));
+            session.setConfig(config);
+            //设置密码
+            session.setPassword(password);
+            //连接  超时时间30s
+            session.connect(3_000);
+
+            //设置cookie
+            Cookie cookie = new Cookie("username", username);
+            cookie.setPath("/");
+            cookie.setMaxAge(30 * 60 * 1000);
+            Cookie passwordCookie = new Cookie("password", encodeToString);
+            passwordCookie.setPath("/");
+            passwordCookie.setMaxAge(30 * 60 * 1000);
+            response.addCookie(cookie);
+            response.addCookie(passwordCookie);
+        } catch (Exception e) {
+            logger.error("fail, param: {}", map.toString(), e);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        } finally {
+
+            if (session != null) session.disconnect();
+        }
+        // 请求ssh
         return ResponseEntity.ok(maps);
     }
 
